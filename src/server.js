0a5c1fe5f29aff3dc6f407db893df54a8c2282a8
arun-kumar-codes <arun.etech2011@gmail.com>
2025-04-18 17:33:55 +0530

    fixed cors

--- src/server.js
+++ src/server.js
@@ -2,10 +2,15 @@ import dotenv from "dotenv";
 import express from "express"
 import { connectDB } from "./database/db.config.js";
 import segmentRoute from "./routes/segment.route.js"
+import cors from 'cors'
 
 dotenv.config()
 const app = express()
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000', // or '*' for all
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }));
 app.use(express.json());const port = process.env.PORT || 8080;
 
 app.use('/api/v1/segment', segmentRoute)
